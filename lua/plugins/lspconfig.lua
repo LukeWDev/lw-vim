4a8419c8093a1c238cc8559e3eb144b7bfb5ec57
Luke Willis <luke.willis1@googlemail.com>
2025-07-13 13:06:42 +0100

    Added LSP
    + Added Lua Rocks
    + Added Mason, MasonLspConfig and Lsp Config
    + Updated telescope to have nicer UI select

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,56 @@
+return {
+  {
+    {
+      "mason-org/mason.nvim",
+      opts = {},
+    },
+    {
+      "mason-org/mason-lspconfig.nvim",
+      opts = {},
+      dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
+      },
+      config = function()
+        require("mason-lspconfig").setup({
+          ensure_installed = {
+            "lua_ls",
+            "clangd",
+          }
+        })
+      end
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+        local lspconfig = require("lspconfig")
+
+        -- Setup Language Servers
+        lspconfig.lua_ls.setup({})
+        lspconfig.clangd.setup({})
+
+        -- Setup Keymaps
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+        vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+
+        -- 🧠 ADD diagnostic UI config here:
+        vim.diagnostic.config({
+          virtual_text = true,
+          signs = true,
+          underline = true,
+          update_in_insert = false,
+          severity_sort = true,
+        })
+
+        -- Optional: show diagnostics on hover
+        vim.api.nvim_create_autocmd("CursorHold", {
+          callback = function()
+            vim.diagnostic.open_float(nil, { focus = false })
+          end,
+        })
+
+    end
+  }
+}
