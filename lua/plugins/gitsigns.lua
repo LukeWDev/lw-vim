682bfcca12ecff6af0635d79db07d56a3ed08c0b
Luke Willis <luke.willis1@googlemail.com>
2025-07-17 18:58:43 +0100

    updated git signs

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,6 +3,7 @@ return {
   config = function()
     require("gitsigns").setup()
 
-    vim.keymap.set('n', "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+    vim.keymap.set('n', "<leader>gph", ":Gitsigns preview_hunk<CR>", {})
+    vim.keymap.set('n', "<leader>gdt", ":Gitsigns diffthis<CR>", {})
   end
 }
