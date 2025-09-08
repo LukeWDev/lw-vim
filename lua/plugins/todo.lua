c15368ff200442c93cf8243c83e16451c5a838a1
Luke Willis <luke.willis1@googlemail.com>
2025-09-08 22:20:13 +0100

    udpate d todo

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -1,12 +1,22 @@
 return {
-  "folke/todo-comments.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    require("todo-comments").setup({
-       search = {
-        exclude_dirs = { "Vendor" },
-      },
-      vim.keymap.set("n", "<leader>td", "<cmd>TodoLocList<CR>", {})
-    })
-  end
+	"folke/todo-comments.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		require("todo-comments").setup({
+			search = {
+				command = "rg",
+				args = {
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--glob",
+					"!Vendor/*",
+				},
+				pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+			},
+			vim.keymap.set("n", "<leader>td", "<cmd>TodoLocList<CR>", {}),
+		})
+	end,
 }
