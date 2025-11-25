982577bfcc95e5aad2e274c1148a94bd6e661ce8
Luke Willis <luke.willis1@googlemail.com>
2025-11-25 21:56:09 +0000

    updated to lldb dap

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -9,8 +9,9 @@ return {
 		opts = {
 			handlers = {},
 			ensure_installed = {
-				"codelldb",
+				"lldb",
 			},
+			automatic_setup = true,
 		},
 	},
 	{
@@ -24,44 +25,102 @@ return {
 			local dapui = require("dapui")
 			dapui.setup({
 				controls = {
-					enabled = true,
 					element = "repl",
+					enabled = true,
 					icons = {
-						pause = "",
-						play = "",
+						disconnect = "",
+						pause = "",
+						play = "",
+						run_last = "",
+						step_back = "",
 						step_into = "",
-						step_over = "",
 						step_out = "",
-						step_back = "",
-						run_last = "↻",
-						terminate = "■",
+						step_over = "",
+						terminate = "",
 					},
+				},
+				element_mappings = {},
+				expand_lines = true,
+				floating = {
+					border = "single",
+					mappings = {
+						close = { "q", "<Esc>" },
+					},
+				},
+				force_buffers = true,
+				icons = {
+					collapsed = "",
+					current_frame = "",
+					expanded = "",
+				},
+				layouts = {
+					{
+						elements = {
+							{
+								id = "scopes",
+								size = 0.25,
+							},
+							{
+								id = "stacks",
+								size = 0.25,
+							},
+						},
+						position = "left",
+						size = 40,
+					},
+					{
+						elements = {
+							{
+								id = "repl",
+								size = 0.5,
+							},
+							{
+								id = "console",
+								size = 0.5,
+							},
+						},
+						position = "bottom",
+						size = 10,
+					},
+				},
+				mappings = {
+					edit = "e",
+					expand = { "<CR>", "<2-LeftMouse>" },
+					open = "o",
+					remove = "d",
+					repl = "r",
+					toggle = "t",
+				},
+				render = {
+					indent = 1,
+					max_value_lines = 100,
 				},
 			})
 
-			dap.adapters.codelldb = {
+			dap.adapters.lldb = {
 				type = "executable",
-				command = "codelldb",
+				command = "/usr/bin/lldb-dap-18",
+				name = "lldb",
 			}
 
 			dap.configurations.cpp = {
 				{
 					name = "Launch file",
-					type = "codelldb",
+					type = "lldb",
 					request = "launch",
 					program = function()
-						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/Binaries/", "file")
+						-- return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/Build/", "file")
+						local file = vim.fn.input("Executable: ")
+						return vim.fn.getcwd() .. "/Build/" .. file .. "/" .. file
 					end,
 					cwd = "${workspaceFolder}",
 					stopOnEntry = false,
 				},
 			}
 
-			dap.listeners.before.attach.dapui_config = function()
-				dapui.open()--
-			end
-			dap.listeners.before.launch.dapui_config = function()
+			dap.listeners.after.event_initialized.dapui_config = function()
 				dapui.open()
+				print("after_event_init")
 			end
 			dap.listeners.before.event_terminated.dapui_config = function()
 				dapui.close()
